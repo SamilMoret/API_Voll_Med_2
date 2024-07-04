fdb2a2a4252d92bb8633af31bf79075b3e70d839
Samil <samil.moret@gmail.com>
2024-07-04 08:42:52 -0300

    Adicionando ResponseEntity

---
+++ api/src/main/java/med/voll/api/dadosdetalhamentopaciente/DadosDetalhamentoPaciente.java
@@ -0,0 +1,10 @@
+package med.voll.api.dadosdetalhamentopaciente;
+
+import med.voll.api.endereco.Endereco;
+import med.voll.api.paciente.Paciente;
+
+public record DadosDetalhamentoPaciente(String nome, String email, String telefone, String cpf, Endereco endereco) {
+    public DadosDetalhamentoPaciente(Paciente paciente) {
+        this(paciente.getNome(), paciente.getEmail(), paciente.getTelefone(), paciente.getCpf(), paciente.getEndereco());
+    }
+}
